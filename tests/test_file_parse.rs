8b2a97c50f8e737f5ec8d97b33eabb0362e55189
Clifford Richardson <cmrallen+birdhalfbaked@gmail.com>
2026-07-14 23:48:23 +0200

    update Readme with example on the caveat of forcing users to manage lifetime of records after parse

--- tests/test_file_parse.rs
+++ tests/test_file_parse.rs
@@ -6,17 +6,25 @@ use std::path::Path;
 #[test]
 fn test_parse_file() {
     let path = Path::new("data/FAACIFP18.txt");
-    let file = File::open(path).unwrap();
+    let file = File::open(path).expect("Failed to open file");
     let parser = Arinc424Parser::new(Arinc424Version::Rev18FAA);
     let reader = BufReader::new(file);
     for line in reader.lines() {
-        let line = line.unwrap();
+        let line = match line {
+            Ok(line) => line,
+            Err(e) => {
+                println!("Error: {:?}", e);
+                continue;
+            }
+        };
         if line.len() == 0 {
             continue;
         }
         let record = parser.parse(line.as_bytes());
         match record {
-            Ok(_) => {}
+            Ok(_) => {
+                // do stuff
+            }
             Err(e) => println!("Error: {:?}", e),
         }
     }
